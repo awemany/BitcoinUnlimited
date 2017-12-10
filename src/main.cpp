4ab7555c601d9b5304f66ade9ff6fc833d7ab018
Awemany <awemany@protonmail.com>
2017-12-10 12:25:13 +0100

    weakblocks: Allow weakblocks to pass validation logic
    In the next commits, the code to mark them properly will be added.

--- src/main.cpp
+++ src/main.cpp
@@ -1668,7 +1668,7 @@ bool ReadBlockFromDisk(CBlock &block, const CDiskBlockPos &pos, const Consensus:
     }
 
     // Check the header
-    if (!CheckProofOfWork(block.GetHash(), block.nBits, consensusParams))
+    if (!CheckProofOfWork(block.GetHash(), WeakBlockProofOfWork(block.nBits), consensusParams))
         return error("ReadBlockFromDisk: Errors in block header at %s", pos.ToString());
 
     return true;
@@ -3980,7 +3980,8 @@ bool FindUndoPos(CValidationState &state, int nFile, CDiskBlockPos &pos, unsigne
 bool CheckBlockHeader(const CBlockHeader &block, CValidationState &state, bool fCheckPOW)
 {
     // Check proof of work matches claimed amount
-    if (fCheckPOW && !CheckProofOfWork(block.GetHash(), block.nBits, Params().GetConsensus()))
+    if (fCheckPOW &&
+        !CheckProofOfWork(block.GetHash(), WeakBlockProofOfWork(block.nBits), Params().GetConsensus()))
         return state.DoS(50, error("CheckBlockHeader(): proof of work failed"), REJECT_INVALID, "high-hash");
 
     // Check timestamp

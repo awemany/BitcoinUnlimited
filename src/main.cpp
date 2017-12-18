1f518acd5211199a10bf761f3b2bd6666324684a
Awemany <awemany@protonmail.com>
2017-12-18 12:35:04 +0100

    weakblocks: Block headers are fine with either min or full POW
    FIXME: Make sure that this is always the case.

--- src/main.cpp
+++ src/main.cpp
@@ -3507,7 +3507,8 @@ bool CheckBlockHeader(const CBlockHeader &block, CValidationState &state, bool f
 {
     // Check proof of work matches claimed amount
     if (fCheckPOW &&
-        !CheckProofOfWork(block.GetHash(), MinWeakblockProofOfWork(block.nBits), Params().GetConsensus()))
+        !CheckProofOfWork(block.GetHash(), MinWeakblockProofOfWork(block.nBits), Params().GetConsensus()) &&
+        !CheckProofOfWork(block.GetHash(), block.nBits, Params().GetConsensus()))
         return state.DoS(50, error("CheckBlockHeader(): proof of work failed"), REJECT_INVALID, "high-hash");
 
     // Check timestamp
